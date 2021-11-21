0a33cdcce606a1f37656593e51a650dda038c619
antoninaorobets <pani.antonina.don@gmail.com>
2021-11-21 12:03:50 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const dodger = document.getElementById("dodger")
+
+document.addEventListener("keydown", function (event) {
+
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    };
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    };
+  });
+
+  
+function moveDodgerLeft() {
+        const leftPosition = dodger.style.left.replace("px","");
+        const left = parseInt(leftPosition);
+        if (left > 0) {  
+            dodger.style.left = `${left -1}px`;
+        };  
+}
+
+function moveDodgerRight() {
+  
+    const check = dodger.style.left
+    const rightPosition =dodger.style.left.replace("px","");
+    const rigt = parseInt(rightPosition);
+
+     if (rigt < 360) {
+        dodger.style.left = `${rigt+1}px`;
+     }
+}
